f0ef55f9ba5a4be91b0db89e23dadc192001fd9c
Jon Chen <sjchen@sjchen.com>
2012-07-25 13:09:44 -0400

    footer, css changes, added -4 -6, but will need to rethink how that should work.

--- application/application.py
+++ application/application.py
@@ -3,16 +3,32 @@ from pbs import mtr
 
 app = Flask(__name__)
 
+def clientIP():
+	userIP = request.environ.get('REMOTE_ADDR')
+	return userIP
+
 @app.route('/')
 def displayMTR():
-	clientIP = request.environ.get('REMOTE_ADDR')
+	clientIP = clientIP()
 	reportMTR = mtr("-r","-w",clientIP).stdout
 	return "<pre>" + reportMTR + "</pre>"
 
 @app.route('/c')
 def displayMTRBrowser():
-	clientIP = request.environ.get('REMOTE_ADDR')
+	clientIP = clientIP()
 	reportMTR = mtr("-r","-w",clientIP).stdout
+	return reportMTR
+
+@app.route('/c4')
+def displayMTRv4():
+	clientIP = clientIP()
+	reportMTR = mtr("-r","-w","-4",clientIP).stdout
+	return reportMTR
+
+@app.route('/c6')
+def displayMTRv4():
+	clientIP = clientIP()
+	reportMTR = mtr("-r","-w","-6",clientIP).stdout
 	return reportMTR
 
 if __name__ == '__main__':
