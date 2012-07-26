d3e1579c0f7c0bac1ae48b1d14e40ab2ee705abc
Jon Chen <sjchen@sjchen.com>
2012-07-26 14:01:40 -0400

    add center.js, fix css

---
+++ static/js/center.js
@@ -0,0 +1,11 @@
+$(window).resize(function(){
+
+        $('.container').css({
+                position:'absolute',
+                left: ($(window).width() - $('.container').outerWidth())/2,
+                top: ($(window).height() - $('.container').outerHeight())/2
+        });
+
+});
+
+$(window).resize();
