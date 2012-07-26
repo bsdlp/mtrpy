2f2a13f79b9c09d977e0d71ea6685f5df426df14
Jon Chen <sjchen@sjchen.com>
2012-07-26 03:57:10 -0400

    revert change to get client IP with a outside function

--- application/application.py
+++ application/application.py
@@ -3,31 +3,27 @@ from pbs import mtr
 
 app = Flask(__name__)
 
-def clientIP():
-	userIP = request.environ.get('REMOTE_ADDR')
-	return userIP
-
 @app.route('/')
 def displayMTR():
-	clientIP = clientIP()
+	clientIP = request.environ.get('REMOTE_ADDR')
 	reportMTR = mtr("-r","-w",clientIP).stdout
 	return "<pre>" + reportMTR + "</pre>"
 
 @app.route('/c')
 def displayMTRBrowser():
-	clientIP = clientIP()
+	clientIP = request.environ.get('REMOTE_ADDR')
 	reportMTR = mtr("-r","-w",clientIP).stdout
 	return reportMTR
 
 @app.route('/c4')
 def displayMTRv4():
-	clientIP = clientIP()
+	clientIP = request.environ.get('REMOTE_ADDR')
 	reportMTR = mtr("-r","-w","-4",clientIP).stdout
 	return reportMTR
 
 @app.route('/c6')
 def displayMTRv4():
-	clientIP = clientIP()
+	clientIP = request.environ.get('REMOTE_ADDR')
 	reportMTR = mtr("-r","-w","-6",clientIP).stdout
 	return reportMTR
 
