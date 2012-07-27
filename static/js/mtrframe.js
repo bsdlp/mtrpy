3276f4c7046170e5447d55ba8adf9f9c4f535171
Jon Chen <sjchen@sjchen.com>
2012-07-27 11:50:09 -0400

    dynamically resize mtr iframe

--- static/js/mtrframe.js
+++ static/js/mtrframe.js
@@ -1,6 +1,43 @@
-var mtrFrameCSS = document.createElement("link")
+/* var mtrFrameCSS = document.createElement("link")
 mtrFrameCSS.href = "/static/css/style.css";
 mtrFrameCSS .rel = "stylesheet";
 mtrFrameCSS .type = "text/css";
-frames['mtrWINDOW'].document.body.appendChild(mtrFrameCSS);
+frames['mtrWINDOW'].document.body.appendChild(mtrFrameCSS); */
 
+$(document).ready(function());
+    {
+        // Set specific variable to represent all iframe tags.
+        var iFrames = $(".mtrWINDOW");
+
+        // Resize heights.
+        function mtrResizeFrame()
+        {
+            $(".mtrWINDOW").style.height = $(".mtrWINDOW").contentWindow.document.body.offsetHeight + 'px';
+        }
+
+        // Check if browser is Safari or Opera.
+        if ($.browser.safari || $.browser.opera)
+        {
+            // Start timer when loaded.
+            $('iframe').load(function()
+                {
+                    setTimeout(mtrResizeFrame, 0);
+                }
+            );
+
+            // Safari and Opera need a kick-start.
+            var mtrFrameSource = $(".mtrWINDOW").src;
+            $(".mtrWINDOW").src = '';
+            $(".mtrWINDOW").src = mtrFrameSource;
+        }
+        else
+        {
+            // For other good browsers.
+            $('iframe').load(function()
+                {
+                    // Set inline style to equal the body height of the iframed content.
+                    this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+                }
+            );
+        }
+    }
