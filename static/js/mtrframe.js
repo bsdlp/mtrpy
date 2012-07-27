91355309af5fcf3a61f4d09fd2c91d5f56b779bd
Jon Chen <sjchen@sjchen.com>
2012-07-27 11:19:47 -0400

    center the mtr iframe, get ready to js it up

---
+++ static/js/mtrframe.js
@@ -0,0 +1,6 @@
+var mtrFrameCSS = document.createElement("link")
+mtrFrameCSS.href = "/static/css/style.css";
+mtrFrameCSS .rel = "stylesheet";
+mtrFrameCSS .type = "text/css";
+frames['mtrWINDOW'].document.body.appendChild(mtrFrameCSS);
+
