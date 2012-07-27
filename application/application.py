464261846da618d689da10917bfde891dd4173d5
Jon Chen <sjchen@sjchen.com>
2012-07-27 08:42:42 -0400

    fix tabbing. sublime text sucks.

--- application/application.py
+++ application/application.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def displayMTR():
-	clientIP = request.environ.get('REMOTE_ADDR')
-	reportMTR = mtr("-r","-w",clientIP).stdout
-	return "<pre>" + reportMTR + "</pre>"
+    clientIP = request.environ.get('REMOTE_ADDR')
+    reportMTR = mtr("-r","-w",clientIP).stdout
+    return "<pre>" + reportMTR + "</pre>"
 
 @app.route('/c')
 def displayMTRBrowser():
-	clientIP = request.environ.get('REMOTE_ADDR')
-	reportMTR = mtr("-r","-w",clientIP).stdout
-	return reportMTR
+    clientIP = request.environ.get('REMOTE_ADDR')
+    reportMTR = mtr("-r","-w",clientIP).stdout
+    return reportMTR
 
 @app.route('/c4')
 def displayMTRv4():
-	clientIP = request.environ.get('REMOTE_ADDR')
-	reportMTR = mtr("-r","-w","-4",clientIP).stdout
-	return reportMTR
+    clientIP = request.environ.get('REMOTE_ADDR')
+    reportMTR = mtr("-r","-w","-4",clientIP).stdout
+    return reportMTR
 
 @app.route('/c6')
 def displayMTRv4():
-	clientIP = request.environ.get('REMOTE_ADDR')
-	reportMTR = mtr("-r","-w","-6",clientIP).stdout
-	return reportMTR
+    clientIP = request.environ.get('REMOTE_ADDR')
+    reportMTR = mtr("-r","-w","-6",clientIP).stdout
+    return reportMTR
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
