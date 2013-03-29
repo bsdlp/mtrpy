c153dead4e69c4424a333d2783f702ec4a81b0e2
Jon Chen <fly@sjchen.net>
2013-03-29 19:16:50 -0400

    print the result

--- app/mtrpy.py
+++ app/mtrpy.py
@@ -4,5 +4,5 @@ from flask import request
 def mtrpy():
     clientIP = request.environ.get('REMOTE_ADDR')
     reportMTR = mtr("-r","-w",clientIP, _bg=True)
-    return reportMTR
+    return reportMTR.wait()
 
