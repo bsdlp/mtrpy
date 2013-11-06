d98e26fb0081e5a21f39baa453ed7f7f582dec6e
Jon Chen <bsd@voltaire.sh>
2013-11-06 11:10:52 -0500

    docstrings and x-forwarded-for

--- application/app/mtrpy.py
+++ application/app/mtrpy.py
@@ -2,7 +2,15 @@ from sh import mtr
 from flask import request
 
 def mtrpy():
-    clientIP = request.environ.get('REMOTE_ADDR')
+    """
+    Janky-ass function for mtr via sh.
+    """
+
+    # janky-ass grab the user's ip addr
+    if not request.headers.getlist("X-Forwarded-For"):
+        clientIP = request.remote_addr
+    else:
+        clientIP = request.headers.getlist("X-Forwarded-For")[0]
+
     reportMTR = mtr("-r","-w",clientIP, _bg=True)
     return reportMTR.wait()
-
