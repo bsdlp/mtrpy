ad7e674c97e05b6600de3becf7a0a0a5422a7293
Jon Chen <bsd@voltaire.sh>
2013-11-14 21:24:38 -0500

    class-ness

--- application/app/mtrpy.py
+++ application/app/mtrpy.py
@@ -1,5 +1,41 @@
+#!/usr/bin/env python
+
 from sh import mtr
 from flask import request
+import socket
+
+class MTR(object):
+    def __init__(self, client_ip=None, user_agent=None):
+        self.client_ip = client_ip
+        self.user_agent = user_agent
+
+    def get_user_agent(self):
+        self.user_agent = str(request.user_agent)
+
+    def get_client_ip(self):
+        if not request.headers.getlist("X-Forwarded-For"):
+            client_ip = request.remote_addr
+        else:
+            client_ip = request.headers.getlist("X-Forwarded-For")[-1]
+
+    def is_curl(self, user_agent):
+        if "curl" in agent:
+            return True
+        else:
+            return False
+
+    def is_powershell(self,user_agent):
+        if "PowerShell" in agent:
+            return True
+        else:
+            return False
+
+    def lookup(addr):
+        try:
+            return socket.gethostbyaddr(addr)
+        except socket.herror:
+            return 'No PTR record for %s' % addr
+
 
 def mtrpy():
     """
@@ -8,10 +44,10 @@ def mtrpy():
 
     # janky-ass grab the user's ip addr
     if not request.headers.getlist("X-Forwarded-For"):
-        clientIP = request.remote_addr
+        client_ip = request.remote_addr
     else:
-        clientIP = request.headers.getlist("X-Forwarded-For")[-1]
+        client_ip = request.headers.getlist("X-Forwarded-For")[-1]
 
-    reportMTR = mtr("-r","-w",clientIP, _bg=True)
+    reportMTR = mtr("-r","-w",client_ip, _bg=True)
     return reportMTR.wait()
 
