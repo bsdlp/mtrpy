6f6cc14a81d09410530448b8d506a5386e6b0eec
Jon Chen <bsd@voltaire.sh>
2014-01-29 00:04:55 -0500

    init mtrwindow

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
-from flask import render_template
 from app import app, mtrpy
+from flask import render_template
+from app.mtrpy import MTR
 
+
+mtr = MTR()
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/mtrwindow')
+def mtrwindow():
+    mtr.get_client_ip()
+    reportMTR = mtr.report_mtr(mtr.client_ip)
+    return render_template('mtrwindow.html',
+                           clientIP = mtr.client_ip,
+                           reportMTR = reportMTR)
 
